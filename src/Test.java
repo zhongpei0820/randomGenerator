4f6c67bc93754b087f6ff73e8f937c40a8c7f8f7
zhongpei0820 <kevinzhongpei@163.com>
2016-10-17 11:19:48 -0400

    Add test from range test

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,8 @@ import java.util.HashSet;
  */
 public class Test {
 
-    private final static int RANGE = 100000000;
+    private final static int RANGE_START = 1000;
+    private final static int RANGE_END = 100000000;
     private final static int NUMBER_OF_ELEMENTS = 10000;
 
 
@@ -22,6 +23,11 @@ public class Test {
 
         //Test runtime memory
         Test.memoryTest();
+
+
+        //Test range
+        Test.rangeTest();
+
     }
 
 
@@ -30,10 +36,10 @@ public class Test {
         numberGenerator nG = new numberGenerator();
 
         System.out.println("Testing runtime...");
-        System.out.println("Generating " + NUMBER_OF_ELEMENTS + " random numbers from 0 to " + RANGE + " ...");
+        System.out.println("Generating " + NUMBER_OF_ELEMENTS + " random numbers from 0 to " + RANGE_END + " ...");
 
         long startTime = System.currentTimeMillis();
-        nG.generateNoDup(RANGE, NUMBER_OF_ELEMENTS);
+        nG.generateNoDup(RANGE_END, NUMBER_OF_ELEMENTS);
         long endTime = System.currentTimeMillis();
 
         System.out.print("The time is ");
@@ -44,20 +50,48 @@ public class Test {
     private static void uniqueTest() {
         numberGenerator nG = new numberGenerator();
         HashSet<Integer> hashSet = new HashSet<>();
+
         System.out.println("Testing duplicates...");
-        int[] random = nG.generateNoDup(RANGE, NUMBER_OF_ELEMENTS);
+
+        int[] random = nG.generateNoDup(RANGE_END, NUMBER_OF_ELEMENTS);
+
         boolean flag = true;
         for (int i = 0; i < random.length; i++) {
             if (!hashSet.add(random[i])) flag = false;
         }
+
         System.out.println("No duplicates :" + flag);
     }
 
     private static void memoryTest() {
         Runtime.getRuntime().gc();
+
         numberGenerator nG = new numberGenerator();
-        nG.generateNoDup(RANGE, NUMBER_OF_ELEMENTS);
+
+        nG.generateNoDup(RANGE_END, NUMBER_OF_ELEMENTS);
+
         System.out.println((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024L * 1024L) + "MB");
+
         Runtime.getRuntime().gc();
+    }
+
+    private static void rangeTest() {
+
+        numberGenerator nG = new numberGenerator();
+
+        System.out.println("Testing Random numbers in the range from " + RANGE_START + " to " + RANGE_END);
+
+        int[] array = nG.generateNoDup(RANGE_START, RANGE_END, NUMBER_OF_ELEMENTS);
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            min = Math.min(min, array[i]);
+            max = Math.max(max, array[i]);
+        }
+
+        boolean res = (min >= 0) && (max <= RANGE_END);
+        System.out.println("Random numbers in the range:" + res);
     }
 }
--- src/numberGenerator.java
+++ src/numberGenerator.java
@@ -5,14 +5,13 @@ import java.util.Random;
  * Created by zhongpei on 2016/10/16.
  */
 public class numberGenerator {
-    Random random;
+    private Random random;
 
     public numberGenerator() {
         random = new Random();
     }
 
-
-    //Generate k random numbers from 1 to n without duplicates.
+    //Generate k random integers from 1 to n.
     public int[] generateNoDup(int n, int k) {
         int[] array = new int[n + 1];
         for (int i = 0; i <= n; i++) {
@@ -20,6 +19,22 @@ public class numberGenerator {
         }
         for (int i = 0; i <= n; i++) {
             int index = random.nextInt(n + 1);
+            int tmp = array[index];
+            array[index] = array[i];
+            array[i] = tmp;
+        }
+        return Arrays.copyOfRange(array, 0, k + 1);
+    }
+
+    //Generate k random integers from start(inclusive) to end(inclusive) without duplicates.
+    public int[] generateNoDup(int start, int end, int k) {
+        int length = end - start + 1;
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = start + i;
+        }
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(length);
             int tmp = array[index];
             array[index] = array[i];
             array[i] = tmp;
